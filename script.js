de3c6597cf42ff21db962ba58d27f15c29cba8d3
pavlyuk04 <alex.pavlyuk04@gmail.com>
2021-08-26 14:49:19 +0300

    added test task, but think I can do better

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+  $(document).ready(function(){
+    $('.test__img').slick({
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+    })
+  })
+}
